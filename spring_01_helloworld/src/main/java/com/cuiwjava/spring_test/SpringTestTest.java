f0d2a4abdab047712cdc508aa2274f90599a3827
cuiwei34 <cuiwei34@jd.com>
2019-04-01 14:29:54 +0800

    引用包问题修复

--- spring_01_helloworld/src/main/java/com/cuiwjava/spring_test/SpringTestTest.java
+++ spring_01_helloworld/src/main/java/com/cuiwjava/spring_test/SpringTestTest.java
@@ -1,5 +1,6 @@
 package com.cuiwjava.spring_test;
 
+import com.cuiwjava.hello.HelloWorld;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +17,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 // SpringJUnit4 运行
 @RunWith(SpringJUnit4ClassRunner.class)
 // 上下文配置对象
-@ContextConfiguration("classpath:springtest.xml")
+@ContextConfiguration("classpath:applicationContext.xml")
 public class SpringTestTest {
-
+    // 按照bean类型自动注入
     @Autowired
     private SomeBean someBean;
+
+    @Autowired
+    private HelloWorld helloWorld;
 
     @Test
     public void testIoC(){
         someBean.doWork();
+        helloWorld.sayHello();
     }
 }

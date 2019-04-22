6643514e3550bb8d05078e669d220afb91982b95
cuiwjava <cuiwjava@163.com>
2019-04-23 07:44:22 +0800

    动态代理

---
+++ spring_05_proxy_jdk/src/test/java/AppTest.java
@@ -0,0 +1,33 @@
+import com.cuiwjava.domain.Employee;
+import com.cuiwjava.service.IEmployeeService;
+import com.cuiwjava.tx.TransactionManagerAdvice;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Classname AppTest
+ * @Description TODO
+ * @Date 2019/4/22/022 0:09
+ * @Created by cuiwjava
+ */
+//@SpringJUnitConfig
+@ContextConfiguration("classpath:AppTest-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class AppTest {
+    @Autowired
+    private TransactionManagerAdvice transactionManagerAdvice;
+
+    @Test
+    public void testSave(){
+        IEmployeeService proxyObject = transactionManagerAdvice.getProxyObject();
+        proxyObject.save(new Employee());
+        System.out.println(proxyObject);
+    }
+
+    public void testUpdate(){
+
+    }
+}

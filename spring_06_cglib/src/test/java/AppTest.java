5cd187e424c33ed4f87cc8efcb4f615c2a5e88e5
cuiwjava <cuiwjava@163.com>
2019-05-01 15:47:30 +0800

    cglib 动态代理

--- spring_06_cglib/src/test/java/AppTest.java
+++ spring_06_cglib/src/test/java/AppTest.java
@@ -1,9 +1,11 @@
 import com.cuiwjava.domain.Employee;
 import com.cuiwjava.service.IEmployeeService;
+import com.cuiwjava.service.impl.EmployeeServiceImpl;
 import com.cuiwjava.tx.TransactionManagerAdvice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -19,6 +21,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class AppTest {
     @Autowired
     private TransactionManagerAdvice advice;
+    // JDK代理对象 com.sun.proxy.$Proxy$
+    @Test
+    public void testSave() throws Exception{
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,"D:/test");
+        EmployeeServiceImpl proxy = advice.getProxyObject();
+        System.out.println(proxy);
+    }
+
+    @Test
+    public void testUpdate(){
+
+    }
 
 
 }
